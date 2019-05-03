1892deac7ece583631ad362c112720f6dff739e5
Jeff Schreiber <jjpl.schreiber@gmail.com>
2019-05-03 16:30:16 +0200

    Styling

--- recipe.php
+++ recipe.php
@@ -34,33 +34,29 @@
         <div class="recipe-content-ingredient">
           <h3>Ingredienten</h3>
           <ul>
-            <li><p><i>3 el.</i> <b>bruine basterdsuiker</b></p></li>
-            <li><p><i>3 tl.</i> <b>paprikapoeder</b></p></li>
-            <li><p><i>300 ml.</i> <b>ketchup</b></p></li>
-            <li><p><i>3 el.</i> <b>whiskey</b></p></li>
-            <li><p><i>1 st.</i> <b>uien</b></p></li>
-            <li><p><i>1 st.</i> <b>knoflookteen</b></p></li>
-            <li><p><i>2 el.</i> <b>worcestershire sauce</b></p></li>
-            <li><p><i>5 dr.</i> <b>tabasco</b></p></li>
-            <li><p><i>6 el.</i> <b>appelcider azijn</b></p></li>
-            <li><p><i>6 el.</i> <b>balsamico azijn</b></p></li>
-            <li><p><i>1 tl.</i> <b>zwart peper</b></p></li>
+            <li><p>300 ml. <b>ketchup</b></p></li>
+            <li><p>1 st. <b>uien</b></p></li>
+            <li><p>1 st. <b>knoflookteen</b></p></li>
+            <li><p>6 el. <b>appelcider azijn</b></p></li>
+            <li><p>6 el. <b>balsamico azijn</b></p></li>
+            <li><p>3 el. <b>whiskey</b></p></li>
+            <li><p>3 el. <b>bruine basterdsuiker</b></p></li>
+            <li><p>2 el. <b>worcestershire sauce</b></p></li>
+            <li><p>3 tl. <b>paprikapoeder</b></p></li>
+            <li><p>1 tl. <b>zwart peper</b></p></li>
+            <li><p>5 dr. <b>tabasco</b></p></li>
           </ul>
         </div>
         <div class="recipe-content-prepare">
           <h3>Bereiding</h3>
-          <p><b>Stap 1</b></p>
-          <p>Snipper de uien fijn en bak ze op zacht vuur in roomboter, totdat ze uit elkaar vallen en caramel bruin zijn van kleur.</p>
-          <p><b>Stap 2</b></p>
-          <p>Voeg dan volle eetlepels basterd suiker, fijngehakte knoflook en paprikapoeder toe en laat dit smelten en pruttelen voor een paar minuten.</p>
-          <p><b>Stap 3</b></p>
-          <p>Schenk vervolgens de whiskey erbij en laat dit enkele minuten koken zodat de alcohol kan verdampen.</p>
-          <p><b>Stap 4</b></p>
-          <p>Blus de uien af met de Worcestershire sauce, tabasco, appelcider azijn, balsamico azijn en ketchup. Laat het vervolgens vijf tot tien minuten indikken op zacht vuur en roer regelmatig.</p>
-          <p><b>Stap 5</b></p>
-          <p>Kruid alles af met een beetje zout en een flinke hoeveelheid zwarte peper uit de molen.</p>
-          <p><b>Stap 6</b></p>
-          <p>Haal de saus door een fijne zeef en laat het afkoelen in de koelkast.</p>
+          <ol>
+            <li><p>Snipper de uien fijn en bak ze op zacht vuur in roomboter, totdat ze uit elkaar vallen en caramel bruin zijn van kleur.</p></li>
+            <li><p>Voeg dan volle eetlepels basterd suiker, fijngehakte knoflook en paprikapoeder toe en laat dit smelten en pruttelen voor een paar minuten.</p></li>
+            <li><p>Schenk vervolgens de whiskey erbij en laat dit enkele minuten koken zodat de alcohol kan verdampen.</p></li>
+            <li><p>Blus de uien af met de Worcestershire sauce, tabasco, appelcider azijn, balsamico azijn en ketchup. Laat het vervolgens vijf tot tien minuten indikken op zacht vuur en roer regelmatig.</p></li>
+            <li><p>Kruid alles af met een beetje zout en een flinke hoeveelheid zwarte peper uit de molen.</p></li>
+            <li><p>Haal de saus door een fijne zeef en laat het afkoelen in de koelkast.</p></li>
+          </ol>
         </div>
       </div>
     </main>
